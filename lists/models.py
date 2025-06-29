e8fa9df25bdb17d138778860e8f3b13ceca5a889
khoalololo <minhkhoa278kd@gmail.com>
2025-06-29 20:38:28 +0700

    Adjust models so that items are associated with different lists

--- lists/models.py
+++ lists/models.py
@@ -1,5 +1,16 @@
+# lists/models.py
 from django.db import models
 
-# Create your models here.
+class List(models.Model):
+    # This model might be empty for now, or have a name field later
+    pass # Or other fields like 'name = models.CharField(max_length=200)'
+
 class Item(models.Model):
     text = models.TextField(default="")
+    # --- ADD THIS LINE ---
+    list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
+    # --- END ADDITION ---
+
+    def __str__(self):
+        # Good practice: add a __str__ method for better representation in admin/shell
+        return self.text
--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
@@ -11,28 +11,44 @@ class HomePageTest(TestCase):
 
     def test_renders_input_form(self):
         response = self.client.get("/")
-        #self.assertContains(response, '<form method="POST">')
+        # The book will guide you on when to uncomment and change this action
+        # self.assertContains(response, '<form method="POST">')
         self.assertContains(response, '<form method="POST" action="/lists/new">')
         self.assertContains(response, '<input name="item_text"')
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        mylist=List()
+        mylist.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = mylist # Assign the List object to the item's 'list' ForeignKey
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = mylist # Assign the same List object to the second item
         second_item.save()
+
+        saved_list=List.objects.get() # Assumes only one list exists for simplicity here
+        self.assertEqual(saved_list, mylist) # Check if the retrieved list is the same object
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
 
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
+
+        # CORRECTED LINES:
+        # Assertions for the first item's text and its linked list object
         self.assertEqual(first_saved_item.text, "The first (ever) list item")
+        self.assertEqual(first_saved_item.list, mylist) # Correct: Compare Item.list (List object) to mylist (List object)
+
+        # Assertions for the second item's text and its linked list object
         self.assertEqual(second_saved_item.text, "Item the second")
+        self.assertEqual(second_saved_item.list, mylist) # Correct: Compare Item.list (List object) to mylist (List object)
 
 class NewListTest(TestCase):
     def test_can_save_a_POST_request(self):
@@ -51,13 +67,15 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
     def test_renders_input_form(self):
         response = self.client.get("/lists/the-only-list-in-the-world/")
-        #self.assertContains(response, '<form method="POST">')
+        # The book will guide you on when to uncomment and change this action
+        # self.assertContains(response, '<form method="POST">')
         self.assertContains(response, '<form method="POST" action="/lists/new">')
         self.assertContains(response, '<input name="item_text"')
 
     def test_displays_all_list_items(self):
-        Item.objects.create(text="itemey 1")
-        Item.objects.create(text="itemey 2")
+        mylist = List.objects.create()
+        Item.objects.create(text="itemey 1", list=mylist)
+        Item.objects.create(text="itemey 2", list=mylist)
 
         response = self.client.get("/lists/the-only-list-in-the-world/")
 
